2e073199a922081ef6ab4eb5731e7817b5de098b
Bibek Sah <bkumarsah627@rku.ac.in>
2022-08-10 05:28:23 +0530

    Added Successfully

---
+++ combobox/combobox/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox1.Text);
+            string[] items = { };
+            comboBox1.Items.AddRange(items);
+            MessageBox.Show("Value Added");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(comboBox1.Text);
+            string[] items = { };
+            comboBox1.Items.AddRange(items);
+            MessageBox.Show("Value Added");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.SelectedIndex.ToString());
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comboBox1.SelectedItem.ToString());
+
+        }
+    }
+}
